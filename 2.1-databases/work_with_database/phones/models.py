24ab64864928a3ec5c746d47da1f7c09e107e962
dima <vadim.vadim4@yandex.ru>
2022-09-14 04:49:55 +0300

    done

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,13 @@
+from email.mime import image
+from platform import release
 from django.db import models
-
+from django.template.defaultfilters import slugify
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50, blank=True, null=True)
+    price = models.BigIntegerField(blank=True, null=True)
+    image = models.ImageField(max_length=250, blank=True, null=True)
+    release_date = models.DateField(max_length=50, blank=True, null=True)
+    lte_exists = models.BooleanField(max_length=50, blank=True, null=True)
+    slug = models.SlugField(max_length=50, blank=True, null=True)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,25 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_type = str(request.GET.get('sort'))
+
+    if sort_type == 'name':
+        phones = Phone.objects.order_by('name')[0:]
+    elif sort_type == 'min_price':
+        phones = Phone.objects.order_by('price')[0:]
+    elif sort_type == 'max_price':
+        phones = Phone.objects.order_by('-price'[0:])
+    else:
+        phones = Phone.objects.all()
+
+    context = {'phones': phones}
+
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
